be6fa369fa558a52cd864cfab695aeb067a03c4c
MagaMiqaelyan <48835022+MagaMiqaelyan@users.noreply.github.com>
2019-08-23 17:55:06 +0400

    my client

---
+++ MyClient/MyClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MyClient.ServiceReferenceHTTP;
+
+namespace MyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            string greeting = Console.ReadLine();
+            ContractClient contract = new ContractClient();
+            Console.WriteLine(contract.GetMessage(greeting));
+        }
+    }
+}
